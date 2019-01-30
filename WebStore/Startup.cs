e00e38060c8f21b77a794251212cbd112f15f8a4
Stanislav Konomenov <stkon@rambler.ru>
2019-01-31 00:47:28 +0300

    Первая фиксация. Старт проекта.

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -6,11 +6,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace WebStore
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; } 
+
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -25,9 +33,12 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
+            var replayStr = Configuration["CustomReplayString"];
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(replayStr);
             });
         }
     }
