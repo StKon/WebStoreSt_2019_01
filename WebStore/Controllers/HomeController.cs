e49b23b6b644ac566b19d8ea26fbc6139c75743a
Stanislav Konomenov <stkon@rambler.ru>
2019-01-31 03:49:14 +0300

    Первое представление. Модель данных.

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        //локальный источник данных
+        private static List<EmployeeViewModel> _employee = new List<EmployeeViewModel>
+        {
+            new EmployeeViewModel {Id = 1, FirstName = "Иван", SecondName = "Иванов", Patronymic = "Иванович", Age = 22},
+            new EmployeeViewModel {Id = 2, FirstName = "Владислав", SecondName = "Петров", Patronymic = "Иванович", Age = 35}
+            new EmployeeViewModel {Id = 3, FirstName = "Станислав", SecondName = "Сидоров", Patronymic = "Петрович", Age = 53}
+        };
+
+        public IActionResult Index()
+        {
+            
+            //return Content("Hello from controller");
+            //return Accepted();
+            //return BadRequest();
+            //return NotFound();
+            //return Json();
+            //return PartialView();
+            return View();
+        }
+    }
+}
--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -12,8 +12,15 @@ namespace WebStore
 {
     public class Startup
     {
-        private IConfiguration Configuration { get; } 
+        /// <summary>
+        /// Добавляем свойство для доступа к конфигурации
+        /// </summary>
+        private IConfiguration Configuration { get; }
 
+        /// <summary>
+        /// Добавляем новый конструктор, принимающий интерфейс IConfiguration
+        /// </summary>
+        /// <param name="configuration"></param>
         public Startup(IConfiguration configuration)
         {
             this.Configuration = configuration;
@@ -23,6 +30,8 @@ namespace WebStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Добавляем сервисы, необходимые для mvc
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,13 +42,23 @@ namespace WebStore
                 app.UseDeveloperExceptionPage();
             }
 
-            var replayStr = Configuration["CustomReplayString"];
+            //Добавляем расширение для использования статических файлов (скрипты, html, css файлы)
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            //Добавляем обработку запросов в mvc-формате
+            app.UseMvc(routes =>
             {
-                //await context.Response.WriteAsync("Hello World!");
-                await context.Response.WriteAsync(replayStr);
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //var replayStr = Configuration["CustomReplayString"];
+            //
+            //app.Run(async (context) =>
+            //{
+            //    //await context.Response.WriteAsync("Hello World!");
+            //    await context.Response.WriteAsync(replayStr);
+            //});
+
         }
     }
 }
