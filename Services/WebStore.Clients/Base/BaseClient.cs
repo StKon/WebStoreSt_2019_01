c48d6ebdac8f9b4bd649b19fb60582102b65ad76
Stanislav Konomenov <stkon@rambler.ru>
2019-03-02 11:18:25 +0300

    Создание тестового клиента

---
+++ Services/WebStore.Clients/Base/BaseClient.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace WebStore.Clients.Base
+{
+    public abstract class BaseClient
+    {
+        protected readonly HttpClient _client;       //клиент
+        public string ServicesAddress { set; get; }  //адрес сервера
+
+        /// <summary> Конструктор </summary>
+        public BaseClient(IConfiguration configuration)
+        {
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(configuration["ClientAddress"])  //адрес веб-службы
+            };
+
+            //конфигурация клиента
+            _client.DefaultRequestHeaders.Clear();  //Очищаем заголовок
+            //ответ в формате json
+            _client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
---
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
+using WebStore.Interfaces.Api;
+
+namespace WebStore.Clients.Values
+{
+    public class ValuesClient : BaseClient, IValuesService
+    {
+        public ValuesClient(IConfiguration configuration) : base(configuration)
+        {
+            ServicesAddress = "api/values";  //адрес сервиса, values - контроллер
+        }
+
+        public IEnumerable<string> Get() => GetAsync().Result;
+
+        public Task<IEnumerable<string>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Get(int id) => GetAsync(id).Result;
+
+        public Task<string> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Uri Post(string value) => PostAsync(value).Result;
+
+        public Task<Uri> PostAsync(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpStatusCode Put(int id, string value) => PutAsync(id, value).Result;
+
+        public Task<HttpStatusCode> PutAsync(int id, string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpStatusCode Delete(int id) => DeleteAsync(id).Result;
+
+        public Task<HttpStatusCode> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
