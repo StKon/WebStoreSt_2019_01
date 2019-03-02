a6e79a4bdb2ba1b33f16da5374ac039584f33778
Stanislav Konomenov <stkon@rambler.ru>
2019-03-02 12:51:42 +0300

    Клиент для тестового контроллера Values + вызов его в веб-приложении

--- Services/WebStore.Clients/Values/ValuesClient.cs
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -18,37 +19,56 @@ namespace WebStore.Clients.Values
 
         public IEnumerable<string> Get() => GetAsync().Result;
 
-        public Task<IEnumerable<string>> GetAsync()
+        public async Task<IEnumerable<string>> GetAsync()
         {
-            throw new NotImplementedException();
+            var list = new List<string>();
+            //запрос
+            var response = await _client.GetAsync($" {ServicesAddress} ");
+            //успешный результат
+            if (response.IsSuccessStatusCode)
+            {
+                list = await response.Content.ReadAsAsync<List<string>>();
+            }
+            return list;
         }
 
         public string Get(int id) => GetAsync(id).Result;
 
-        public Task<string> GetAsync(int id)
+        public async Task<string> GetAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = string.Empty;
+            var response = await _client.GetAsync($"{ServicesAddress}/get/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                result = await response.Content.ReadAsAsync<string>();
+            }
+            return result;
         }
 
         public Uri Post(string value) => PostAsync(value).Result;
 
-        public Task<Uri> PostAsync(string value)
+        public async Task<Uri> PostAsync(string value)
         {
-            throw new NotImplementedException();
+            var response = await _client.PostAsJsonAsync($"{ServicesAddress}/post", value);
+            response.EnsureSuccessStatusCode();  //успешно
+            return response.Headers.Location;    //вернуть адрес ресурса
         }
 
         public HttpStatusCode Put(int id, string value) => PutAsync(id, value).Result;
 
-        public Task<HttpStatusCode> PutAsync(int id, string value)
+        public async Task<HttpStatusCode> PutAsync(int id, string value)
         {
-            throw new NotImplementedException();
+            var response = await _client.PutAsJsonAsync($"{ServicesAddress}/put/{id}", value);
+            response.EnsureSuccessStatusCode();
+            return response.StatusCode;  //статусный код
         }
 
         public HttpStatusCode Delete(int id) => DeleteAsync(id).Result;
 
-        public Task<HttpStatusCode> DeleteAsync(int id)
+        public async Task<HttpStatusCode> DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var response = await _client.DeleteAsync($"{ServicesAddress}/delete/{id}");
+            return response.StatusCode;
         }
     }
 }
